6298e10e0e37ff38402b7b2d8fad594c455810a1
kaine <ghanirafli8@gmail.com>
2023-03-24 10:59:47 +0700

    rough to do with local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,86 @@
-import { useState, useEffect } from 'react';
+import React, { useEffect, useState } from "react";
 
-function App() {
-
-  return(
-    <>
-      h
-    </>
-  )
-
+interface Todo {
+  id: number;
+  task: string;
+  completed: boolean;
 }
+
+const initialTodos: Todo[] = [];
+
+const App = () => {
+  const [todos, setTodos] = useState<Todo[]>(initialTodos);
+  const [newTodo, setNewTodo] = useState<string>("");
+
+  const addTodo = (task: string) => {
+    const newTodo = {
+      id: Date.now(),
+      task,
+      completed: false,
+    };
+    const updatedTodos = [...todos, newTodo];
+    setTodos(updatedTodos);
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+    setNewTodo("");
+  };
+
+  const deleteTodo = (id: number) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+  };
+
+  const updateTodoStatus = (id: number, completed: boolean) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed } : todo
+    );
+    setTodos(updatedTodos);
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+  };
+
+  const handleAddTodo = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!newTodo) return;
+    addTodo(newTodo);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTodo(event.target.value);
+  };
+
+  useEffect(() => {
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <form onSubmit={handleAddTodo}>
+        <input type="text" value={newTodo} onChange={handleChange} />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={(event) =>
+                updateTodoStatus(todo.id, event.target.checked)
+              }
+            />
+            <span style={{ textDecoration: todo.completed ? "line-through" : "" }}>
+              {todo.task}
+            </span>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
