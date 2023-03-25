d1abda2a0800af9371a26f81c890d244a363bbfa
kaine <ghanirafli8@gmail.com>
2023-03-26 05:58:31 +0700

    separate task and tamagochi components

---
+++ src/components/tamagotchi.tsx
@@ -0,0 +1,12 @@
+export const Tamagotchi = () => {
+    return(
+        <>
+            <p className="todo-status">
+                finish task when?
+                <br/>
+                status effect
+            </p>
+            this is tamagotchi placeholder
+        </>
+    )
+}
---
+++ src/components/task.tsx
@@ -0,0 +1,56 @@
+import { NavLink } from "react-router-dom";
+import { taskProps } from "../interface";
+
+const DateElementCheck = ({todo}:{todo:string}) =>{
+    if(todo){
+    return(
+        <>
+            {todo} - {todo}
+        </>
+    )
+    }else{
+    return(
+        <>no date?</>
+    )
+    }
+}
+
+export const Task = ({todo, update, handle, deleteTask}: taskProps) => {
+    return(
+        <>
+            <li key={todo.id}>
+                <input
+                    className="todo-checkbox"
+                    type="checkbox"
+                    checked={todo.completed}
+                    onChange={(event) =>
+                        update(todo.id, event.target.checked)
+                    }
+                />
+                <NavLink style={{width:'100%', textDecoration:'none'}} to={`${todo.id}`}>
+                    <div
+                        className="todo-task"
+                        style={{
+                        textDecoration: todo.completed ? "line-through" : "",
+                        }}
+                    >
+                        <div
+                            onClick={() => {
+                                const newTask = prompt("Enter new task", todo.task);
+                                if (newTask) {
+                                handle(todo.id, newTask.trim());
+                                }
+                            }}
+                        >
+                            {todo.task}
+                        </div>
+                        <div className='todo-task-date'>
+                        <DateElementCheck todo={todo.date}/>
+                        </div>
+                    </div>
+                </NavLink>
+                <div className="todo-delete-wrapper"><button className="todo-delete" onClick={() => deleteTask(todo.id)}>Delete</button></div>
+            </li>
+        </>
+    )
+}
