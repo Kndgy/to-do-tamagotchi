8dfff9bc0e6b3ee096e06ac73e10e1519eb16c2b
kaine <ghanirafli8@gmail.com>
2023-04-05 21:34:47 +0700

    map deadline and plain deadline to be used for

--- src/components/tamagotchi.tsx
+++ src/components/tamagotchi.tsx
@@ -1,4 +1,11 @@
+import { useTodos } from "../utils/state"
+
 export const Tamagotchi = () => {
+    const {todos} = useTodos()
+    // console.log(todos)
+    console.log("plain deadline", todos.map((item)=>item.plainDeadline))
+    console.log("deadline string", todos.map((item)=>item.deadline))
+    console.log("total task/feeds", todos.length)
     return(
         <>
             <p className="todo-status">
